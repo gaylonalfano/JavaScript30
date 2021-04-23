520648bc5f8e239f1b695e9dc837c1527fbd2396
gaylonalfano <gaylon.alfano@gmail.com>
2021-04-23 11:48:57 +0800

    Basic idea is to first grab all links on the page. Then we apply a mouseevent event listener on each. We also create a separate <span> element to be our highlight and add it to the DOM. Then, the mouseevent handler grabs the coordinates of the specific link and then applies some styling to our highlight span element by matching the width, height, top, left of the link element. This then makes our highlight effect follow along on the page. Read comments for more details.

---
+++ 22 - Follow Along Link Highlighter/main.js
@@ -0,0 +1,38 @@
+var triggers = document.querySelectorAll("a");
+// Create a highlight span element
+var highlight = document.createElement("span");
+// Add a CSS class of 'highlight' to this element;
+highlight.classList.add("highlight");
+// Insert into the DOM. Adds <span> to bottom of body
+// NOTE Can't see it bc it doesn't have width/height
+document.body.append(highlight);
+// Listen for when user enters a link
+function highlightLink() {
+    // console.log(this, "highlight!");
+    // Next, need to find the width/height of the element we're hovering over
+    // and where on the page is it located using this.getBoundingClientRect();
+    var linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    // Now that we have coords let's apply styles to our highlight to match
+    // Method 1: Pure CSS properties
+    // NOTE Only need left and top (right/bottom alone doesn't work!). Don't need all.
+    // highlight.style.width = `${linkCoords.width}px`;
+    // highlight.style.height = `${linkCoords.height}px`;
+    // highlight.style.left = `${linkCoords.left}px`;
+    // highlight.style.right = `${linkCoords.right}px`;
+    // highlight.style.top = `${linkCoords.top}px`;
+    // highlight.style.bottom = `${linkCoords.bottom}px`;
+    // Method 2: Using Transform property to apply for smoother animation
+    highlight.style.width = linkCoords.width + "px";
+    highlight.style.height = linkCoords.height + "px";
+    // NOTE You only need top/left
+    highlight.style.transform = "translate(" + (linkCoords.left + window.scrollX) + "px, " + (linkCoords.top + window.scrollY) + "px)";
+    // IMPORTANT It seems to work but if you scroll down a little then it's off.
+    // You can compensate using two approaches:
+    // 1. You can compute the offset of the PARENT element
+    // 2. Compute how far the user has scrolled down and take away from trigger
+}
+// Listen for mouseenter event on each link/trigger
+triggers.forEach(function (trigger) {
+    trigger.addEventListener("mouseenter", highlightLink);
+});
